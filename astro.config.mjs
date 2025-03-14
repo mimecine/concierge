62d621719fe9a0d38d1d064c60dd1d7f30576905
Marco Romeny <marco@mimecom.net>
2025-03-14 16:56:04 +0000

    Allsorts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        react(),
-        tailwind({
-            applyBaseStyles: false
-        }),
-        alpinejs({ entrypoint: '/src/astroentry.js' })
-    ],
+    integrations: [react(), alpinejs({ entrypoint: '/src/astroentry.js' })],
     output: 'static',
     adapter: netlify({
         imageCDN: false
-    })
+    }),
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
